a0a72767825cf02ca1281e9d8494c2abf99dd17d
GeunHye <wheeinjj417@gmail.com>
2025-06-04 01:41:09 +0900

    -mainPage 리다이렉트 수정

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -22,7 +22,7 @@ const startApiUrl = window.baseUrls.start;
 
 //처음으로 버튼
 backBtn.addEventListener('click', () => {
-  window.location.href = `${startApiUrl}/mainPage`; 
+  window.location.href = `/mainPage`; 
 });
 
 // // 현재 URL 가져오기
