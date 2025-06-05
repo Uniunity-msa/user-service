c036ce32746d0718cf90633e9d95ba063a177c5f
GeunHye <wheeinjj417@gmail.com>
2025-06-05 15:26:11 +0900

    -db 환경변수 수정

--- main.js
+++ main.js
@@ -16,11 +16,11 @@ app.get('/ready', async (req, res) => {
   try {
     // MySQL 연결 검사
     const dbConn = await mysql.createConnection({
-      host: process.env.MYSQL_HOST,
-      port: process.env.MYSQL_PORT,
-      user: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DATABASE,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       connectTimeout: 2000  // 연결 타임아웃 2초
     });
 
