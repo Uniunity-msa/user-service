e3b15d9d153f8822d2a4ff159f727e39382a43d5
GeunHye <wheeinjj417@gmail.com>
2025-06-05 13:43:56 +0900

    -포트번호 3004명시

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./main");
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+app.listen(3004, () => {
   console.log(`User service listening on http://localhost:${PORT}`);
 });
